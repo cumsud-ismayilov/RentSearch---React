a55ec8de203006daa2947a9153d6c100f24785c5
Your Name <you@example.com>
2023-05-01 22:21:43 +0400

    navbar start

---
+++ reactproject/src/App.js
@@ -0,0 +1,12 @@
+import Layout from "./components/Layout/Layout";
+
+
+function App() {
+  return (
+     <Layout>
+      <h1>salam</h1>
+     </Layout>
+  );
+}
+
+export default App;
---
+++ reactproject/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import '../src/style/global.scss'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../node_modules/bootstrap/dist/js/bootstrap.min";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
