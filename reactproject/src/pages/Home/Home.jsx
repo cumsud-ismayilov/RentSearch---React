89bdb5748962c94d1baad5018bc9bae0bb7449c7
Your Name <you@example.com>
2023-05-02 19:38:30 +0400

    class banner and inner add

--- reactproject/src/pages/Home/Home.jsx
+++ reactproject/src/pages/Home/Home.jsx
@@ -4,7 +4,8 @@ import {MdOutlineCompareArrows} from "react-icons/md";
 import { HiArrowSmDown } from "react-icons/hi";
 const Home = () => {
   return (
-   <section className='banner'>
+   <main>
+      <section className='banner'>
       <div className='container'>
         <div className='row'>
           <div className='col-xl-6 g-5'>
@@ -28,6 +29,76 @@ const Home = () => {
         </div>
       </div>
    </section>
+   <section className='inner'>
+      <div className='container'>
+         <div className='row'>
+            <div className='col-xl-6 g-5'>
+               <div className='bestmoment'>
+                  <p>Be there the moment renters need you.</p>
+                  <h2>Convert more leads by driving traffic from Google directly to your property website. </h2>
+                  <button>Get RentSearch.</button>
+               </div>
+            </div>
+            <div className='col-xl-6 g-5'>
+               <div className='engines'>
+               <div className='engine'>
+                   <p>87%</p>
+                   <h3>of renters start their search on a search engine.¹</h3>
+                </div>
+               <div className='engine'>
+                   <p>350+</p>
+                   <h3>monthly in-market renters directed to a property’s website. ²</h3>
+                </div>
+               <div className='engine'>
+                   <p>3X</p>
+                   <h3>higher click-through rates than the industry average.³</h3>
+                </div>
+               <div className='engine'>
+                   <p>10%</p>
+                   <h3>Lower cost-per-lead than the industry average.⁴</h3>
+                </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   <section className='marketplace'>
+      <div className='container'>
+         <div className='row'>
+            <div className='col-xl-6 g-5'>
+               <div className='marketbackground'>
+                  <img src="https://solutions.rent.com/wp-content/uploads/2022/10/2-bedroom-apartment-in-Atlanta-2.png"/>
+               <div className='services-indicator'>
+                  <p>More page views when combined with RentMarketplace.⁵ </p>
+                  <h4>95%</h4>
+               </div>
+               <div className='services-indicator2'>
+                  <h2>Average clicks per property per month.⁶</h2>
+                  <h5>402</h5>
+               </div>
+               </div>
+            </div>
+            <div className='col-xl-6 g-5'>
+               <div className='award'>
+                  <div className='reactword'>
+                  <h1><MdOutlineCompareArrows  color='#fff' size={19} /></h1>
+                  <h2>Rent<span>Search.<sup>TM</sup></span></h2>
+                  </div>
+                  <p>Award-winning Google search campaigns that keep occupancy rates high.</p>
+                  <div className='target'>
+                     <h3>Target Effectively</h3>
+                     <h4>Unparalleled ad targeting thanks to our leading technology and proprietary first-party data from our marketplaces.</h4>
+                  </div>
+                  <h5>- Boost Performance</h5>
+                  <h5>- Reduce Risk</h5>
+                  <button>Get RentSearch.</button>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   </main>
+   
   )
 }
 
